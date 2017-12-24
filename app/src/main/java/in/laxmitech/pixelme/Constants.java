c809da12ef1d222b6f9280d562c74a5372f3cb46
sandipsavani2009 <deepanshi1503@gmail.com>
2017-12-24 17:05:58 +0530

    Changes

--- app/src/main/java/in/laxmitech/pixelme/Constants.java
+++ app/src/main/java/in/laxmitech/pixelme/Constants.java
@@ -6,14 +6,7 @@ package in.laxmitech.pixelme;
 
 public interface Constants {
 
-    interface TestAds {
-        String BANNER_UNIT_ID = "ca-app-pub-3940256099942544/6300978111";
-        String INTERSTITIAL_UNIT_ID = "ca-app-pub-3940256099942544/1033173712";
-    }
-
-    interface Ads {
-        String APP_ID = "ca-app-pub-4341497827318637~9829863616";
-        String BANNER_UNIT_ID = "ca-app-pub-4341497827318637/4306074190";
-        String INTERSTITIAL_UNIT_ID = "ca-app-pub-4341497827318637/6141900042";
+    interface Storage {
+        String DIR_NAME = "PixelMe";
     }
 }
--- app/src/main/java/in/laxmitech/pixelme/ui/HomeActivity.java
+++ app/src/main/java/in/laxmitech/pixelme/ui/HomeActivity.java
@@ -1,11 +1,19 @@
 package in.laxmitech.pixelme.ui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -18,6 +26,10 @@ import com.google.android.gms.ads.reward.RewardedVideoAd;
 import com.google.android.gms.ads.reward.RewardedVideoAdListener;
 import com.warkiz.widget.IndicatorSeekBar;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
+import in.laxmitech.pixelme.Constants;
 import in.laxmitech.pixelme.R;
 import in.laxmitech.pixelme.ads.AdsHelper;
 import nl.dionsegijn.pixelate.OnPixelateListener;
@@ -25,7 +37,9 @@ import nl.dionsegijn.pixelate.Pixelate;
 
 public class HomeActivity extends BaseActivity implements View.OnClickListener {
 
+    private final int PERMISSION_REQUEST_CODE = 12;
     private final int IMAGE_REQUEST_CODE = 11;
+
     private ImageView mImageView;
     private IndicatorSeekBar mDensitySeekbar;
     private RewardedVideoAd mRewardedVideoAd;
@@ -45,8 +59,6 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
     private void loadAds() {
         AdView adView = (AdView) findViewById(R.id.adView);
         AdsHelper.showBanner(adView);
-
-//        AdsHelper.showInterstitialAd(this);
 
 //        AdsHelper.showRewardVideoAd(this);
         mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this);
@@ -97,8 +109,12 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
         mImageView = (ImageView) findViewById(R.id.imgView);
         mDensitySeekbar = (IndicatorSeekBar) findViewById(R.id.density_seekbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.home_open_fab);
-        fab.setOnClickListener(this);
+        ImageView openFab = (ImageView) findViewById(R.id.home_open_fab);
+        openFab.setOnClickListener(this);
+        ImageView ShareFab = (ImageView) findViewById(R.id.home_share_fab);
+        ShareFab.setOnClickListener(this);
+        ImageView saveFab = (ImageView) findViewById(R.id.home_save_fab);
+        saveFab.setOnClickListener(this);
 
         mDensitySeekbar.setOnSeekChangeListener(getSeekChangeListener());
 
@@ -111,7 +127,85 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
                 onOpenFileClicked();
                 break;
 
+            case R.id.home_save_fab:
+                onSaveFileClicked();
+                break;
+
+            case R.id.home_share_fab:
+                opShareFileClicked();
+                break;
+
         }
+    }
+
+    private void onSaveFileClicked() {
+        if (isPermissionGranted()) {
+            saveImage();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void saveImage() {
+        if (mImageView.getDrawable() != null) {
+            BitmapDrawable draw = (BitmapDrawable) mImageView.getDrawable();
+            Bitmap bitmap = draw.getBitmap();
+
+            if (bitmap != null) {
+                File sdCard = Environment.getExternalStorageDirectory();
+                File dir = new File(sdCard.getAbsolutePath() + File.separator + Constants.Storage.DIR_NAME);
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                }
+
+                String fileName = String.format("%d.jpg", System.currentTimeMillis());
+                File outFile = new File(dir, fileName);
+
+                FileOutputStream outStream = null;
+                try {
+                    outStream = new FileOutputStream(outFile);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+
+                    outStream.flush();
+                    outStream.close();
+
+                    showMessage("Your Pixel photo saved!");
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    showMessage("Could not save photo!");
+                }
+            }
+        }
+
+        AdsHelper.showInterstitialAd(this);
+    }
+
+    private void showMessage(String msg) {
+        if (TextUtils.isEmpty(msg)) {
+            msg = "Something went wrong!";
+        }
+        Snackbar.make(findViewById(R.id.home_parent), msg, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void opShareFileClicked() {
+        if (mImageView.getDrawable() != null) {
+            Drawable mDrawable = mImageView.getDrawable();
+            Bitmap bitmap = ((BitmapDrawable) mDrawable).getBitmap();
+
+            if (bitmap != null) {
+                String path = MediaStore.Images.Media.insertImage(getContentResolver(),
+                        bitmap, "Image Description", null);
+                Uri uri = Uri.parse(path);
+
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_STREAM, uri);
+                startActivity(Intent.createChooser(intent, "Share Photo"));
+            }
+        }
+
+        AdsHelper.showInterstitialAd(this);
     }
 
     private void onOpenFileClicked() {
@@ -134,7 +228,21 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
                 case IMAGE_REQUEST_CODE:
                     setImageChosen(data.getData());
                     break;
+
             }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    saveImage();
+                } else {
+                    showMessage("Write permission required to save photo");
+                }
+                break;
         }
     }
 
@@ -215,5 +323,20 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
         mRewardedVideoAd.destroy(this);
         super.onDestroy();
 
+    }
+
+    private boolean isPermissionGranted() {
+        boolean isGranted = false;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                isGranted = true;
+            }
+        } else {
+            isGranted = true;
+        }
+
+        return isGranted;
     }
 }
